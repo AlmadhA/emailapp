90f3b4142581bbcf951fd4cdfec8442b9ebb9dd8
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-16 16:04:23 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -119,9 +119,7 @@ grid_options = gb.build()
 
 AgGrid(
     pivot1,
-    gridOptions=grid_options,
-    fit_columns_on_grid_load=False,
-    allow_unsafe_jscode=True,# Nonaktifkan fit otomatis pada grid load
+    gridOptions=grid_options,# Nonaktifkan fit otomatis pada grid load
     height=400,
 )
 st.dataframe(pivot1.fillna(0), use_container_width=True, hide_index=True)
