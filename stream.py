a572e5096a9e51ff9ec2d67a4265f1d73c20b9d6
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-16 16:57:11 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -88,7 +88,7 @@ df_mie['Tanggal'] = pd.to_datetime(df_mie['BULAN'], format='%B %Y')
 df_mie['BULAN'] = pd.Categorical(df_mie['BULAN'], categories=df_mie.sort_values('Tanggal')['BULAN'].unique(), ordered=True)
 df_mie = df_mie[df_mie['BULAN']>='January 2024']
 pivot1=df_mie.pivot(index='Nama Cabang', columns='BULAN', values='Kuantitas').reset_index().fillna(0)
-pivot1.iloc[:,1:] = pivot1.iloc[:,1:].astype('float64')
+pivot1.iloc[:,1:] = pivot1.iloc[:,1:].astype('int')
 
 
 gb = GridOptionsBuilder.from_dataframe(pivot1)
