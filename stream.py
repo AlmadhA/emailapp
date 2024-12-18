112a1cac7fac8a0330338e80dd74baafdfd917ab
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-18 08:46:15 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -122,7 +122,7 @@ row_colors = pivot1.iloc[:, 1:].apply(lambda row: row_gradient_colors(row, cmap)
 # Menambahkan cellStyle untuk setiap kolom numerik
 for col_idx, col in enumerate(pivot1.columns[1:]):
     gb.configure_column(
-        col, width=150,
+        col, width=200,
         cellStyle=JsCode(f"""
         function(params) {{
             const colors = {row_colors.apply(lambda x: x[col_idx]).tolist()};
