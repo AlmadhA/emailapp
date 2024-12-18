2ea5bfbf570e3f6636966a1f5fc7045de131bf43
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-18 10:08:39 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -145,7 +145,6 @@ gb.configure_columns(column_defs)
 gb.configure_default_column(resizable=True)
 gb.configure_grid_options(domLayout='normal') 
 gb.configure_default_column(filterable=True, sortable=True) 
-gb.configure_grid_options(alwaysShowHorizontalScroll = True)
 
 
 grid_options = gb.build()
@@ -154,7 +153,7 @@ total = pd.DataFrame((pivot1.iloc[:,1:].sum(axis=0).values).reshape(1,len(pivot1
 total['Nama Cabang'] ='TOTAL'
 AgGrid(pivot1,
     #pd.concat([pivot1,total], ignore_index=True),
-    gridOptions=grid_options,  fit_columns_on_grid_load=True,
+    gridOptions=grid_options,  fit_columns_on_grid_load=False,
     allow_unsafe_jscode=True)
 
 st.dataframe(total.loc[:,[total.columns[-1]]+total.columns[:-1].to_list()], use_container_width=True, hide_index=True)
