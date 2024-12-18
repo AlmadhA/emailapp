ca08ec5d96916650853c2fce6287968cbcdb62a8
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-18 10:30:02 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -147,7 +147,7 @@ total = pd.DataFrame((pivot1.iloc[:,1:].sum(axis=0).values).reshape(1,len(pivot1
 total['Nama Cabang'] ='TOTAL'
 AgGrid(pd.concat([pivot1,total], ignore_index=True),
     gridOptions=grid_options,  fit_columns_on_grid_load=False, domLayout='normal',
-    allow_unsafe_jscode=True, columns_auto_size_mode=ColumnsAutoSizeMode.FIT_ALL_COLUMNS_TO_VIEW )
+    allow_unsafe_jscode=True, columns_auto_size_mode=ColumnsAutoSizeMode.NO_AUTOSIZE )
 
 st.dataframe(total.loc[:,[total.columns[-1]]+total.columns[:-1].to_list()], use_container_width=True, hide_index=True)
 df_mie3 = df_mie.merge(df_days, how='left')
