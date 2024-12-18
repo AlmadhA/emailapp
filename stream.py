ca35157def7d7af988adc59856f51f148125728e
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-18 10:03:45 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -146,7 +146,7 @@ gb.configure_columns(column_defs)
 #gb.configure_column(pivot1.columns[0], pinned="left",  filter="text", autoSizeColumns=True)
 #gb.configure_default_column(resizable=True)
 #gb.configure_grid_options(domLayout='normal') 
-#gb.configure_default_column(filterable=True, sortable=True)   
+gb.configure_default_column(filterable=True, sortable=True)   
 
 grid_options = gb.build()
 
