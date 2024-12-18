27d6e325a972196067c7e26fa1bf106b4389e808
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-18 10:21:20 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -139,7 +139,7 @@ for col_idx, col in enumerate(pivot1.columns[1:]):
 
 gb.configure_column(pivot1.columns[0], pinned="left",  filter="text")
 gb.configure_default_column(resizable=True,filterable=True, sortable=True)
-
+gb.configure_grid_options(suppressColumnToolPanel=True)
 grid_options = gb.build()
 
 total = pd.DataFrame((pivot1.iloc[:,1:].sum(axis=0).values).reshape(1,len(pivot1.columns)-1),columns=pivot1.columns[1:])
