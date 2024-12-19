35c46cb6837e68c216a9d5082861edb00139ce32
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-19 09:11:52 +0700

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -1,8 +1,9 @@
 import streamlit as st
-
+from streamlit_extras.metric_cards import style_metric_cards
 # Gambar atau ikon untuk mempercantik tampilan
 st.image("https://www.example.com/icon.png", width=50)
 
 # Menampilkan metrik dengan desain lebih menarik
 st.metric(label="Profit", value="$15,000", delta="+25%", delta_color="inverse")
 st.metric(label="New Users", value="850", delta="-5%", delta_color="normal")
+style_metric_cards()
