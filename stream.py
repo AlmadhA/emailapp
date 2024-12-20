23c021f9872872feab737dfd6600bd9f710d391f
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-20 10:15:08 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -63,7 +63,8 @@ def create_dual_axis_chart(data, x_column, y_bar_column, y_line_column, title):
         template="plotly_white",
         margin=dict(l=50, r=50, t=40, b=40),
         paper_bgcolor="white",  # Warna background luar (canvas), termasuk margin
-        plot_bgcolor="white"
+        plot_bgcolor="white",
+        width=600
     )
     return fig
 
