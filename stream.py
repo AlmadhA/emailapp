511de7d0192a040b1b348d26c1f6a4c9bd8c870e
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-20 14:45:32 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -152,7 +152,8 @@ with col[0]:
             {   background-color: white;
                 border: 1px solid rgba(49, 51, 63, 0.2);
                 border-radius: 0.5rem;
-                padding: calc(1em - 1px)
+                padding: calc(1em - 1px);
+                color:'#143d59'
             }
             """,
     ):
