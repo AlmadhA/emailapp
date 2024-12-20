e34642b2949577a6030d5aa941a977a73e82bc7a
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-20 14:47:21 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -153,7 +153,7 @@ with col[0]:
                 border: 1px solid rgba(49, 51, 63, 0.2);
                 border-radius: 0.5rem;
                 padding: calc(1em - 1px);
-                color:'#143d59'
+                color:#143d59
             }
             """,
     ):
