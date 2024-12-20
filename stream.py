0fd4313257a8733aa3ee34600d2f050f5bc8f510
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-20 10:26:42 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -269,7 +269,7 @@ with stylable_container(
     key='grafik1',
     css_styles="""
         {   background-color: white;
-            border: 1px solid rgba(49, 51, 63, 0.2);
+            #border: 1px solid rgba(49, 51, 63, 0.2);
             border-radius: 0.5rem;
             padding: calc(1em - 1px)
         }
