164da23809a6c52649e079afce0e6666dac94cb5
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-20 11:00:02 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -220,7 +220,7 @@ for col_idx, col in enumerate(pivot1.columns[1:]):
 #gb.configure_grid_options(suppressColumnToolPanel=True)
 header_style = {'backgroundColor': '#143d59', 'color': 'white', 'fontWeight': 'bold'}
 for col in pivot1.columns:
-    gb.configure_column(headerStyle=header_style)
+    gb.configure_column(col, headerStyle=header_style)
 gb.configure_column(pivot1.columns[0], pinned="left",  filter="text", cellStyle={'backgroundColor': '#143d59', 'color': 'white'})
 gb.configure_default_column(resizable=True,filterable=True, sortable=True)
 grid_options = gb.build()
