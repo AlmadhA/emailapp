f28c52ee48219070626a04192e98396bb7d901a8
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2024-12-25 14:06:06 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -324,6 +324,7 @@ css = '''
 
 	.stTabs [data-baseweb="tab"] {
 		height: 30px;
+  		border-radius: 20px 20px 0 0;
         color: white;
         white-space: pre-wrap;
 		background-color: #143d59;
