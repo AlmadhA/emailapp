1aca75b22da6822d21c6ecdd0d80b629592aedf9
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-01-08 17:14:02 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -13,6 +13,7 @@ stream1_url = 'https://raw.githubusercontent.com/Analyst-FPnA/Dashboard-Safety-S
 create_page = st.Page("stream.py", title="Create entry", icon=":material/add_circle:")
 delete_page = st.Page(run_stream_script(stream1_url), title="Delete entry", icon=":material/delete:")
 
-pg = st.navigation([st.expander('Dashboard'),create_page, delete_page])
+pg = st.navigation([with st.expander('Dashboard'):
+                        create_page, delete_page])
 pg.run()
 
