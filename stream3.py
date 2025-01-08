a0426be20ffcc83cca6c3a85f4751519b4f4d4be
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-01-08 15:06:28 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.page_link("https://dashboard-harga-barang.streamlit.app/",label="Home")
+with st.sidebar:
+  with st.expander('Dashboard'):
+    st.markdown(
+        "<a href='"https://dashboard-harga-barang.streamlit.app/' target='_blank' style='text-decoration:none;'>"
+        "<div style='background-color:#982B1C;padding:20px;text-align:center;border-radius:10px;color:black; font-family:Roboto,sans-serif; font-size: 16px;'>"
+        "<strong style='color:white;'>Analisis Harga Barang</strong></div>"
+        "</a>",
+        unsafe_allow_html=True,
+    )
+    
