e2c66c58bb53230fbb91c73c0fdfefdffca9c237
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-01-08 14:20:28 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -8,9 +8,3 @@ if st.sidebar.button('Harga Barang'):
     webbrowser.open('https://dashboard-harga-barang.streamlit.app/', new=0)
 if st.sidebar.button('Safety Stock'):
     webbrowser.open('https://dashboard-safetystock.streamlit.app/', new=0)
-
-# Menambahkan HTML di sidebar untuk mengarahkan ke aplikasi lain
-components.html("""
-    <a href="https://dashboard-harga-barang.streamlit.app/" target="_self">Aplikasi Streamlit Lain 1</a><br>
-    <a href="https://dashboard-safetystock.streamlit.app/" target="_self">Aplikasi Streamlit Lain 2</a><br>
-""")
