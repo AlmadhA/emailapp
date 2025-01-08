658d20adb32fa5d9648a0d9c7ba1260aca13b5ba
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-01-08 16:47:41 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -8,15 +8,7 @@ def run_stream_script(url):
         exec(response.text, globals())
     else:
         st.error(f"Failed to download file: {response.status_code}")
+st.page_link("stream.py", label="Home", icon="🏠")
 
-# Arahkan ke aplikasi berdasarkan pilihan pengguna
-def run_lead():
-    stream1_url = 'https://raw.githubusercontent.com/Analyst-FPnA/Leadtime/main/Internal.py'
-    run_stream_script(stream1_url)
-
-create_page = st.Page(run_lead, title="Create entry", icon=":material/add_circle:")
-delete_page = st.page_link("stream.py", label="Home", icon="🏠")
-  
-pg = st.navigation([create_page, delete_page])
 
 
