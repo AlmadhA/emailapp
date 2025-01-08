e7e10f603449f98526e6f07fc6db3ab0361f1e1d
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-01-08 14:19:59 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import webbrowser
+import streamlit.components.v1 as components
 st.sidebar.title('Dashboard')
 
 # Menambahkan beberapa link ke GitHub lainnya di sidebar
@@ -7,3 +8,9 @@ if st.sidebar.button('Harga Barang'):
     webbrowser.open('https://dashboard-harga-barang.streamlit.app/', new=0)
 if st.sidebar.button('Safety Stock'):
     webbrowser.open('https://dashboard-safetystock.streamlit.app/', new=0)
+
+# Menambahkan HTML di sidebar untuk mengarahkan ke aplikasi lain
+components.html("""
+    <a href="https://dashboard-harga-barang.streamlit.app/" target="_self">Aplikasi Streamlit Lain 1</a><br>
+    <a href="https://dashboard-safetystock.streamlit.app/" target="_self">Aplikasi Streamlit Lain 2</a><br>
+""")
