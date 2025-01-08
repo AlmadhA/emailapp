d83aa8ee95cf56e4498cbd30d71298cf72d04c3f
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-01-08 17:00:41 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -1,11 +1,19 @@
 import streamlit as st
 import requests
+
+def run_stream_script(url):
+    # Mengunduh file dari GitHub
+    response = requests.get(url)
+    if response.status_code == 200:
+        # Menjalankan file yang diunduh
+        exec(response.text, globals())
+    else:
+        st.error(f"Failed to download file: {response.status_code}")
+stream1_url = 'https://raw.githubusercontent.com/Analyst-FPnA/Leadtime/main/Internal.py'
 create_page = st.Page("stream.py", title="Create entry", icon=":material/add_circle:")
-delete_page = st.Page("stream2.py", title="Delete entry", icon=":material/delete:")
+delete_page = st.Page(run_stream_script(stream1_url), title="Delete entry", icon=":material/delete:")
 
 pg = st.navigation([create_page, delete_page])
 st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
 pg.run()
-
-
 
