b1a28b0cbd3901a4b4baba5a5fa5ab577af8f5de
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-01-08 16:52:08 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -1,14 +1,11 @@
 import streamlit as st
 import requests
-def run_stream_script(url):
-    # Mengunduh file dari GitHub
-    response = requests.get(url)
-    if response.status_code == 200:
-        # Menjalankan file yang diunduh
-        exec(response.text, globals())
-    else:
-        st.error(f"Failed to download file: {response.status_code}")
-st.page_link("stream.py", label="Home", icon="🏠")
+create_page = st.Page("stream.py", title="Create entry", icon=":material/add_circle:")
+delete_page = st.Page("stream2.py", title="Delete entry", icon=":material/delete:")
+
+pg = st.navigation([create_page, delete_page])
+st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
+pg.run()
 
 
 
