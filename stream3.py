62b983f715c12ab92fde4becabc16df5184b5a28
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-01-08 14:57:29 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -1,15 +1,3 @@
 import streamlit as st
-from st_pages import Page, Section, show_pages, add_page_title, hide_pages
 
-add_page_title()
-
-show_pages(
-    [   
-        Page("https://dashboard-harga-barang.streamlit.app/", "DE Zoomcamp", "💻"),
-
-        # # 2024 Content
-        Section("DE Zoomcamp 2024", "🧙‍♂️"),
-        Page("https://dashboard-harga-barang.streamlit.app/", "📚", in_section=True),
-        Page("https://dashboard-safetystock.streamlit.app/", "Module 1 Introduction & Prerequisites", "1️⃣", in_section=True)])
-
-
+st.page_link(""https://dashboard-harga-barang.streamlit.app/", label="Home", icon="🏠")
