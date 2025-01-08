e535ebc37a61b8a544f58fb391aad977bbe98b5a
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-01-08 17:15:44 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import requests
-az
+
 def run_stream_script(url):
     # Mengunduh file dari GitHub
     response = requests.get(url)
@@ -13,7 +13,6 @@ stream1_url = 'https://raw.githubusercontent.com/Analyst-FPnA/Dashboard-Safety-S
 create_page = st.Page("stream.py", title="Create entry", icon=":material/add_circle:")
 delete_page = st.Page(run_stream_script(stream1_url), title="Delete entry", icon=":material/delete:")
 
-pg = st.navigation([with st.expander('Dashboard'):
-                        create_page, delete_page])
+pg = st.navigation([st.Section('Dashboard'),create_page, delete_page])
 pg.run()
 
