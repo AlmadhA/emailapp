457e65ea7f70da7406d555ddeb3b747a564a3688
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-01-09 08:40:47 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -1,18 +1,15 @@
 import streamlit as st
+import pandas as pd
 import requests
 
-def run_stream_script(url):
-    # Mengunduh file dari GitHub
-    response = requests.get(url)
-    if response.status_code == 200:
-        # Menjalankan file yang diunduh
-        exec(response.text, globals())
-    else:
-        st.error(f"Failed to download file: {response.status_code}")
+df= pd.read_excel('database provinsi.xlsx')
+df
 stream1_url = 'https://raw.githubusercontent.com/Analyst-FPnA/Dashboard-Safety-Stock/main/stream.py'
 create_page = st.Page("stream.py", title="Create entry", icon=":material/add_circle:")
-delete_page = st.Page("../ads_mvn/main/stream.py", title="Delete entry", icon=":material/delete:")
+delete_page = st.Page("stream2.py", title="Delete entry", icon=":material/delete:")
 
 pg = st.navigation([create_page, delete_page], expanded=True)
 pg.run()
+
+
 
