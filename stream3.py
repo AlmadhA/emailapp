6a874393972e3872073b744c51197c80041c151a
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-01-09 17:17:35 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -7,8 +7,8 @@ from st_pages import Section
 stream1_url = 'https://raw.githubusercontent.com/Analyst-FPnA/Dashboard-Safety-Stock/main/stream.py'
 create_page = st.Page("stream.py", title="Create entry", icon=":material/add_circle:")
 delete_page = st.Page("stream2.py", title="Delete entry", icon=":material/delete:")
-
-pg = st.navigation([Section('Dashboard'),
+Section('Dashboard')
+pg = st.navigation([
                     create_page, delete_page], expanded=True)
 pg.run()
 
