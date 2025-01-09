a50013c6679bbca7592fbdb90e20e6251bd6de7c
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-01-09 17:22:40 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 
 
-
+a
 stream1_url = 'https://raw.githubusercontent.com/Analyst-FPnA/Dashboard-Safety-Stock/main/stream.py'
 create_page = st.Page("stream.py", title="Create entry", icon=":material/add_circle:")
 delete_page = st.Page("stream2.py", title="Delete entry", icon=":material/delete:")
