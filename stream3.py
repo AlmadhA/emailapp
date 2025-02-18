19467432e6b561318044fac7e17a36322b5cb976
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-02-18 16:38:35 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -12,7 +12,26 @@ df = pd.DataFrame(data)
 
 # Membaca file GeoJSON provinsi Indonesia
 # Pastikan Anda memiliki file GeoJSON yang sesuai dengan data provinsi Indonesia
-with urlopen('https://github.com/superpikar/indonesia-geojson/blob/master/indonesia-province.json?raw=true') as response:
-    geojson_data = json.load(response)
-#geojson_data
 
+geojson_data = pd.DataFrame(requests.get('https://github.com/superpikar/indonesia-geojson/blob/master/indonesia-province.json').json())
+# Inisialisasi peta
+m = folium.Map(location=[-0.4471383, 117.1655734], zoom_start=3)
+#geojson_data
+# Menambahkan choropleth dengan data harga
+folium.Choropleth(
+    geo_data=geojson_data,
+    name='choropleth',
+    data=df,
+    columns=['Provinsi', 'Rata-rata Harga'],
+    key_on='feature.properties.Propinsi',  # Sesuaikan dengan nama properti di GeoJSON
+    fill_color='YlOrRd',  # Warna gradient
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name='Rata-rata Harga Barang',
+).add_to(m)
+
+# Menambahkan kontrol layer
+folium.LayerControl().add_to(m)
+
+# Menyimpan peta ke file HTML
+#m.save('peta_harga_barang_indonesia.html')
