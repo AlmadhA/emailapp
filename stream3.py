e78555ab26ec72e64022399f0bb70695bf053481
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-02-18 16:45:12 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -12,8 +12,8 @@ df = pd.DataFrame(data)
 
 # Membaca file GeoJSON provinsi Indonesia
 # Pastikan Anda memiliki file GeoJSON yang sesuai dengan data provinsi Indonesia
-geojson_data = pd.DataFrame(requests.get('https://github.com/superpikar/indonesia-geojson/blob/master/indonesia-province.json?raw=true').json())
-geojson_data
+with urlopen('https://github.com/superpikar/indonesia-geojson/blob/master/indonesia-province.json?raw=true') as response:
+    geojson_data = json.load(response)
 # Inisialisasi peta
 m = folium.Map(location=[-0.4471383, 117.1655734], zoom_start=3)
 #geojson_data
