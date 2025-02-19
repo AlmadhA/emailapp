5f28815d3078ebe5690550495839aa143f9a0be5
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-02-19 09:25:17 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -39,7 +39,7 @@ folium.Choropleth(
 
 # Menambahkan tooltip untuk menampilkan rata-rata harga ketika kursor diarahkan
 for feature in geojson_data['features']:
-    province_name = feature['Propinsi']
+    province_name = feature['properties']['Propinsi']
     # Cari rata-rata harga untuk provinsi ini dari DataFrame
     price = df[df['Provinsi'] == province_name]['Rata-rata Harga'].values[0]
     
