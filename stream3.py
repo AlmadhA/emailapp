3c795b98860fb139ff49d5151046d749805a0f1c
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-02-19 10:26:54 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -7,8 +7,8 @@ import streamlit as st
 
 # Contoh DataFrame yang berisi nama provinsi dan rata-rata harga
 data = {
-    'Provinsi': ['JAWA BARAT', 'JAWA TIMUR'],
-    'Rata-rata Harga': [15000, 12000]
+    'Provinsi': ['JAWA BARAT', 'JAWA TIMUR', 'JAWA TENGAH', PROBANTEN],
+    'Rata-rata Harga': [15000, 12000, 14000,9000]
 }
 
 df = pd.DataFrame(data)
