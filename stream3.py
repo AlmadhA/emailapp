168136dc4f28bfddc5d14d8bf6c6d7ac9f9fc62d
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-02-19 09:42:43 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -1,29 +1,38 @@
-import streamlit as st
 import folium
-import pandas as pd
-import json
-from urllib.request import urlopen
 import requests
-from streamlit_folium import folium_static
+import pandas as pd
 from folium import GeoJsonTooltip
 
-# Contoh DataFrame yang berisi nama provinsi dan rata-rata harga
-data = {
-    'Provinsi': ['JAWA BARAT', 'JAWA TIMUR'],
-    'Rata-rata Harga': [15000, 12000]
-}
+# Contoh DataFrame (df berisi data dengan kolom 'Provinsi' dan 'Rata-rata Harga')
+# df = pd.DataFrame({
+#     'Provinsi': ['Aceh', 'Bali', 'Jakarta', ...],
+#     'Rata-rata Harga': [10000, 15000, 20000, ...]
+# })
 
-df = pd.DataFrame(data)
-
-# Membaca file GeoJSON provinsi Indonesia
-# Pastikan Anda memiliki file GeoJSON yang sesuai dengan data provinsi Indonesia
-#with urlopen('https://github.com/superpikar/indonesia-geojson/blob/master/indonesia-province.json?raw=true') as response:
-#    geojson_data = json.load(response)# Inisialisasi peta
-    
+# Buat peta
 m = folium.Map(location=[-0.4471383, 117.1655734], zoom_start=5)
+
+# Mendapatkan data geojson
 geojson_data = requests.get(
     "https://github.com/superpikar/indonesia-geojson/blob/master/indonesia-province.json?raw=true"
 ).json()
+
+# Gabungkan data GeoJSON dengan DataFrame df berdasarkan provinsi
+# Asumsikan 'Propinsi' adalah kolom di GeoJSON yang berisi nama provinsi yang sama dengan kolom di df
+
+# Pastikan bahwa nama kolom 'Provinsi' di df sama dengan 'Propinsi' di GeoJSON
+df['Provinsi'] = df['Provinsi'].str.title()  # Menyamakannya dengan format nama di GeoJSON
+
+# Gabungkan df ke dalam GeoJSON berdasarkan 'Provinsi' dan 'Propinsi'
+geojson_data_with_prices = []
+for feature in geojson_data['features']:
+    provinsi = feature['properties']['Propinsi']
+    harga = df.loc[df['Provinsi'] == provinsi, 'Rata-rata Harga'].values
+    if harga:
+        feature['properties']['Rata-rata Harga'] = harga[0]
+    geojson_data_with_prices.append(feature)
+
+geojson_data['features'] = geojson_data_with_prices
 
 # Menambahkan choropleth dengan data harga
 folium.Choropleth(
@@ -37,7 +46,8 @@ folium.Choropleth(
     line_opacity=0.2,
     legend_name='Rata-rata Harga Barang',
 ).add_to(m)
- 
+
+# Menambahkan GeoJson dengan Tooltip
 folium.GeoJson(
     geojson_data,
     name="Provinsi",
@@ -47,8 +57,13 @@ folium.GeoJson(
         localize=True
     )
 ).add_to(m)
+
 # Menambahkan kontrol layer
 folium.LayerControl().add_to(m)
+
+# Menampilkan peta
+from streamlit_folium import folium_static
 folium_static(m)
+
 # Menyimpan peta ke file HTML
-#m.save('peta_harga_barang_indonesia.html')
+# m.save('peta_harga_barang_indonesia.html')
