ceaf8332d96809f2ab7ae908802d5756679030dc
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-02-27 08:09:15 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -48,7 +48,7 @@ go = GridOptionsBuilder.from_dataframe(df)
 go.configure_side_bar()
 go.configure_column("Pendapatan", aggFunc="sum")  # Menambahkan fungsi agregasi
 go.configure_column("Biaya", aggFunc="sum")
-go.configure_column("Tahun", rowGroup=True)  # Kolom 'Tahun' akan menjadi row group
+go.configure_column("Tahun", rowGroup=True, rowDrag= True)  # Kolom 'Tahun' akan menjadi row group
 go.configure_column("Kategori", rowGroup=True)  # Kolom 'Kategori' akan menjadi row group
 go=go.build()
 go['enableRowGroup'] = True
