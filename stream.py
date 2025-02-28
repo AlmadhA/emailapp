5428208aa1b45eadb33031d8fdded0656451979b
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-02-28 10:28:17 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -19,9 +19,38 @@ import streamlit as st
 from st_aggrid import AgGrid, GridOptionsBuilder
 from streamlit_elements import elements, mui
 
-elements(
-    mui.Button("Click me"),
-)
+with elements("multiple_children"):
+
+    # You have access to Material UI icons using: mui.icon.IconNameHere
+    #
+    # Multiple children can be added in a single element.
+    #
+    # <Button>
+    #   <EmojiPeople />
+    #   <DoubleArrow />
+    #   Hello world
+    # </Button>
+
+    mui.Button(
+        mui.icon.EmojiPeople,
+        mui.icon.DoubleArrow,
+        "Button with multiple children"
+    )
+
+    # You can also add children to an element using a 'with' statement.
+    #
+    # <Button>
+    #   <EmojiPeople />
+    #   <DoubleArrow />
+    #   <Typography>
+    #     Hello world
+    #   </Typography>
+    # </Button>
+
+    with mui.Button:
+        mui.icon.EmojiPeople()
+        mui.icon.DoubleArrow()
+        mui.Typography("Button with multiple children")
 
 # Contoh data
 data = {
