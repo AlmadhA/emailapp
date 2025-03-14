4157216a1940e95ec50695bbc39c6d5a6e375a1f
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-03-14 10:32:52 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -13,7 +13,7 @@ from googleapiclient.discovery import build
 # Use your service account credentials to authenticate
 def authenticate_google_sheets_service_account():
     credentials = service_account.Credentials.from_service_account_file(
-        'service_account.json', scopes=SCOPES)
+        'credentials_shopee.json', scopes=SCOPES)
     return credentials
 
 # Example usage in Streamlit
@@ -90,21 +90,4 @@ def save_attachment(service, msg_id, store_dir='downloads'):
                 with open(file_path, 'wb') as f:
                     f.write(data)
                 print(f'Attachment {file_name} saved to {file_path}')
-
-service = authenticate_gmail(file_json = 'credentials_shopee.json')
-keywords_gojek = ['Mie Gacoan, Batu Tulis','Mie Gacoan, Cibubur','Mie Gacoan, Daan Mogot','Mie Gacoan, Kemang Raya','Mie Gacoan, Tebet',
-            'Mie Gacoan, Padalarang','Mie Gacoan, Manukan','Mie Gacoan, Jatinangor','Mie Gacoan, Semarang Brigjen Sudiarto', 'Mie Gacoan, Mangga Besar']
-keywords_shopee = ['Shopee food - Mie Gacoan - Batu Tulis','Shopee food - Mie Gacoan - Cibubur','Shopee food - Mie Gacoan - Daan Mogot','Shopee food - Mie Gacoan - Kemang Raya','Shopee food - Mie Gacoan - Tebet',
-            'Shopee food - Mie Gacoan - Padalarang','Shopee food - Mie Gacoan - Manukan','Shopee food - Mie Gacoan - Jatinangor','Shopee food - Mie Gacoan - Semarang Brigjen Sudiarto', 'Shopee food - Mie Gacoan - Mangga Besar']
-cab = ['BGRBAT','BKSALT','GGPDAA','KYBKEM','KYBTEB','NPHCIB','SBYTAN','SMDJAT','SMGSUD','TNAMAN']
-
-for i,query in enumerate(keywords_shopee):
-    messages = list_messages(service, query)
-    if messages:
-        print(f'Found {len(messages)} messages.')
-        for msg in messages[:7]:
-            msg_id = msg['id']
-            save_attachment(service, msg_id, store_dir=f'downloads/{cab[i]}')
-    else:
-        print('No messages found with the given criteria.')
 
