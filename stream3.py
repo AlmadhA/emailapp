e49c4c6254a0a56a691f31047d41e34809712058
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-03-14 15:35:19 +0700

    Update stream3.py

--- stream3.py
+++ stream3.py
@@ -9,24 +9,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.errors import HttpError
 from google.auth.transport.requests import Request
 
-from auth0_component import login_button
-import streamlit as st
-from dotenv import load_dotenv
-clientId = 'QwBaicViCKun5FgmCL5l9gWGzDlsjEgp'
-domain = 'dev-oee4zt3daq2ov6tp.us.auth0.com'
 
-
-
-st.title('Welcome to Auth0-Streamlit')
-
-with st.echo():
-    user_info = login_button(clientId=clientId, domain=domain)
-    if user_info:
-        st.write(f'Hi {user_info["nickname"]}')
-        # st.write(user_info) # some private information here
-        
-if not user_info:
-    st.write("Please login to continue")
     
 # Jika memodifikasi scope, hapus file token.json
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.modify']
@@ -45,7 +28,7 @@ def authenticate_gmail(file_json):
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 file_json, SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_local_server(port=0,browser=False)
         
         # Simpan kredensial untuk penggunaan berikutnya
         with open('token.json', 'w') as token:
