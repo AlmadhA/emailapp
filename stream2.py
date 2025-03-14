e21a8fa7d5e7985c69a616adf0561cb45c3b92cf
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-03-14 09:41:35 +0700

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -89,7 +89,7 @@ def authenticate_gmail(file_json):
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 file_json, SCOPES)
-            creds = flow.run_local_server(port=8080)
+            creds = flow.run_local_server(port=8081)
         
         # Simpan kredensial untuk penggunaan berikutnya
         with open('../token.json', 'w') as token:
