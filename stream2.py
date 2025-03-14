db4cf9e5b310c5244f7b5014fa3bb41f94926ddd
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-03-14 09:00:29 +0700

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -27,8 +27,7 @@ def auth_flow():
     auth_code = st.query_params.get("code")
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
         'credentials.json', # replace with you json credentials from your google auth app
-        scopes=["https://www.googleapis.com/auth/userinfo.email", "openid"],
-        redirect_uri=redirect_uri,
+        scopes=["https://www.googleapis.com/auth/userinfo.email", "openid"]
     )
     if auth_code:
         flow.fetch_token(code=auth_code)
